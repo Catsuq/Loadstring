c98bbefdecbe7ef946e9ea9a46f1ffc623c85e8d
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2023-04-28 17:17:42 -0400

    Update DiscordForce.lua
    i made changes to the code that checks if a folder and file exist, and if not, it sends a request to Discord to open an invite link and creates a file. If the request fails, it uses the HttpService to decode the invite link.

--- DiscordForce.lua
+++ DiscordForce.lua
@@ -1,31 +1,36 @@
-local request = (syn and syn.request) or (http and http.request) or http_request	
+-- Fixed Some error thats all.
+-- Rewrite :) Now it fully works.
+local request = (syn and syn.request) or (http and http.request) or http_request
 local HttpService = game:GetService("HttpService")
 
-local TextInFile = "YellowGreg#7993"
-local Discord_Invite = "MzeSqBBpCh"
+local Discord_Invite = "MzeSqBBpCh" -- Change this to your invite code thats all :]
 local FolderName = "Enhanced Software"
-local Folder2 = "/Discord Invites" 
+local Folder2 = "/Discord Invites"
 local FileName = "/Enhanced Software.gg"
 
-if not isfolder(FolderName..Folder2) then
-	makefolder(FolderName..Folder2)
+if not isfolder(FolderName .. Folder2) then
+  makefolder(FolderName .. Folder2)
 end
 
-if not isfile(FolderName..Folder2..FileName) then
-	if request then
-		request({
-			Url = 'http://127.0.0.1:6463/rpc?v=1',
-			Method = 'POST',
-			Headers = {
-			['Content-Type'] = 'application/json',
-			Origin = 'https://discord.com'
-			},
-			Body = HttpService:JSONEncode({
-			cmd = 'INVITE_BROWSER',
-			nonce = HttpService:GenerateGUID(false),
-			args = {code = Discord_Invite}
-			})
-		})
-	end
-	writefile(FolderName..Folder2..FileName, TextInFile)
+if not isfile(FolderName .. Folder2 .. FileName) then
+  if request then
+    request({
+      Url = 'http://127.0.0.1:6463/rpc?v=1',
+      Method = 'POST',
+      Headers = {
+        ['Content-Type'] = 'application/json',
+        Origin = 'https://discord.com'
+      },
+      Body = HttpService:JSONEncode({
+        cmd = 'INVITE_BROWSER',
+        args = {
+          code = Discord_Invite
+        },
+        nonce = HttpService:GenerateGUID(false)
+      })
+    })
+  else
+    game:GetService('HttpService'):JSONDecode(game:HttpGet(('https://discordapp.com/api/v6/invite/' .. Discord_Invite)))
+  end
+  writefile(FolderName .. Folder2 .. FileName, "Enhanced Software")
 end
