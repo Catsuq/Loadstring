674819101b080a66eb7deef3543af07df7b6288d
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2023-05-13 22:50:36 -0400

    Update FPSBooster.lua

--- FPSBooster.lua
+++ FPSBooster.lua
@@ -1,61 +1,150 @@
--- Original script by YellowGreg, modified by MMSVon, Wspboy12, and ShadowClark
--- Created in 5/7/2023 7:17 PM
+--[[ 
+- Original script by YellowGreg, 
+- Modified by MMSVon (added handling for decals), Wspboy12 (optimized code for performance), ShadowClark (added support for particle emitters)
+]]--
+-- Created on 5/7/2023 7:17 PM. Last updated on 5/13/2023 10:45 PM.
 
--- Settings for improving performance
-local decalsYeeted = true -- Setting this to true will remove decals and improve FPS
-local gameDescendants = game:GetDescendants()
+local decalsYeeted = true
 local workspace = game.Workspace
-local lighting = workspace:FindFirstChild("Lighting")
-local terrain = workspace:FindFirstChild("Terrain")
+local lighting = game.Lighting
+local terrain = workspace.Terrain
 
--- Disable unnecessary features to improve performance
-terrain.WaterWaveSize = 0
-terrain.WaterWaveSpeed = 0
-terrain.WaterReflectance = 0
-terrain.WaterTransparency = 0
+-- Improve performance by setting hidden properties only once
+local lightingTechnology = 2
+local decorationEnabled = false
+sethiddenproperty(lighting, "Technology", lightingTechnology)
+sethiddenproperty(terrain, "Decoration", decorationEnabled)
 
-lighting.GlobalShadows = false
+-- Use table instead of separate settings
+local terrainSettings = {
+  WaterWaveSize = 0,
+  WaterWaveSpeed = 0,
+  WaterReflectance = 0,
+  WaterTransparency = 0,
+}
+for setting, value in pairs(terrainSettings) do
+  terrain[setting] = value
+end
+
+lighting.GlobalShadows = 0
 lighting.FogEnd = 9e9
 lighting.Brightness = 0
+settings().Rendering.QualityLevel = "Level01"
 
-settings().Rendering.QualityLevel = Enum.QualityLevel.Level01
-
--- Iterate through all parts, meshparts, and unions to optimize their appearance
-for i, descendant in ipairs(gameDescendants) do
-    if descendant:IsA("BasePart") then
-        descendant.Material = Enum.Material.Plastic
-        descendant.Reflectance = 0
-    elseif descendant:IsA("Decal") and decalsYeeted then
-        descendant.Transparency = 1
-    elseif descendant:IsA("ParticleEmitter") or descendant:IsA("Trail") then
-        descendant.Lifetime = NumberRange.new(0)
-    end
+-- Consolidate common operations into functions for better readability and maintainability
+local function handleBasePart(part)
+  part.Material = Enum.Material.Plastic
+  part.Reflectance = 0
 end
 
--- Check if the device is a 64-bit mobile device
-local is64Bit = system.getInfo("architectureInfo") == "ARM64"
-
-if is64Bit then
-    -- Set the graphics settings to their lowest values
-    graphics.setDrawMode("forceRender")
-    application:setBackgroundColor(0, 0, 0)
-    application:setLogicalDimensions(320, 480)
-    application:setOrientation("portrait")
-    graphics.setConstantAdjustment("stripTexture", true)
-    graphics.setConstantAdjustment("textureWrap", true)
-    graphics.setConstantAdjustment("shader", true)
-    graphics.setDepthBuffer( "off" )
-    graphics.setBlendMode( "replace" )
-    graphics.setLighting( "off" )
-    graphics.setMaterialTexture( "normal", nil )
-    graphics.setMaterialTexture( "specular", nil )
-    graphics.setMaterialTexture( "emission", nil )
-    graphics.setMaterialTexture( "environment", nil )
-    graphics.setMaterialTexture( "reflection", nil )
-    graphics.setMaterialTexture( "fresnel", nil )
-    graphics.setMaterialTexture( "ambient", nil )
-    graphics.setMaterialTexture( "diffuse", nil )
-    graphics.setMaterialTexture( "roughness", nil )
-    graphics.setMaterialTexture( "metalness", nil )
-    graphics.setMaterialTexture( "opacity", nil )
+local function handleDecalOrTexture(decalOrTexture)
+  if decalsYeeted then
+    decalOrTexture.Transparency = 1
+  end
 end
+
+local function handleParticleEmitterOrTrail(emitterOrTrail)
+  emitterOrTrail.Lifetime = NumberRange.new(0)
+end
+
+local function handleExplosion(explosion)
+  explosion.BlastPressure = 1
+  explosion.BlastRadius = 1
+end
+
+local function handleMeshPart(meshPart)
+  if decalsYeeted then
+    meshPart.Material = Enum.Material.Plastic
+    meshPart.Reflectance = 0
+    meshPart.TextureID = 10385902758728957
+  end
+end
+
+local function handleSpecialMesh(specialMesh)
+  if decalsYeeted then
+    specialMesh.TextureId = 0
+  end
+end
+
+local function handleShirtGraphic(shirtGraphic)
+  if decalsYeeted then
+    shirtGraphic.Graphic = ""
+  end
+end
+
+local function handleClothing(clothing)
+  if decalsYeeted then
+    clothing[clothing.ClassName .. "Template"] = ""
+  end
+end
+
+-- Handle existing descendants
+for _, descendant in pairs(workspace:GetDescendants()) do
+  if descendant:IsA("BasePart") and not descendant:IsA("MeshPart") then
+    handleBasePart(descendant)
+  elseif descendant:IsA("Decal") or descendant:IsA("Texture") then
+    handleDecalOrTexture(descendant)
+  elseif descendant:IsA("ParticleEmitter") or descendant:IsA("Trail") then
+    handleParticleEmitterOrTrail(descendant)
+  elseif descendant:IsA("Explosion") then
+    handleExplosion(descendant)
+  elseif descendant:IsA("Fire") or descendant:IsA("SpotLight") or descendant:IsA("Smoke") or descendant:IsA("Sparkles") then
+    descendant.Enabled = false
+  elseif descendant:IsA("MeshPart") then
+    handleMeshPart(descendant)
+  elseif descendant:IsA("SpecialMesh") then
+    handleSpecialMesh(descendant)
+  elseif descendant:IsA("ShirtGraphic") then
+    handleShirtGraphic(descendant)
+  elseif descendant:IsA("Shirt") or descendant:IsA("Pants") then
+    handleClothing(descendant)
+  end
+end
+
+-- Handle new descendants
+workspace.DescendantAdded:Connect(function(descendant)
+  if descendant:IsA("BasePart") and not descendant:IsA("MeshPart") then
+    handleBasePart(descendant)
+  elseif descendant:IsA("Decal") or descendant:IsA("Texture") then
+    handleDecal(descendant)
+  elseif descendant:IsA("ParticleEmitter") or descendant:IsA("Trail") then
+    handleParticleEmitter(descendant)
+  elseif descendant:IsA("Explosion") then
+    handleExplosion(descendant)
+  elseif descendant:IsA("Fire") or descendant:IsA("SpotLight") or descendant:IsA("Smoke") or descendant:IsA("Sparkles") then
+    handleEnabled(descendant)
+  elseif descendant:IsA("MeshPart") then
+    handleMeshPart(descendant)
+  elseif descendant:IsA("SpecialMesh") then
+    handleSpecialMesh(descendant)
+  elseif descendant:IsA("ShirtGraphic") then
+    handleShirtGraphic(descendant)
+  elseif descendant:IsA("Shirt") or descendant:IsA("Pants") then
+    handleClothing(descendant)
+  end
+end)
+
+-- Handle existing descendants
+for _, descendant in ipairs(workspace:GetDescendants()) do
+  if descendant:IsA("BasePart") and not descendant:IsA("MeshPart") then
+    handleBasePart(descendant)
+  elseif descendant:IsA("Decal") or descendant:IsA("Texture") then
+    handleDecal(descendant)
+  elseif descendant:IsA("ParticleEmitter") or descendant:IsA("Trail") then
+    handleParticleEmitter(descendant)
+  elseif descendant:IsA("Explosion") then
+    handleExplosion(descendant)
+  elseif descendant:IsA("Fire") or descendant:IsA("SpotLight") or descendant:IsA("Smoke") or descendant:IsA("Sparkles") then
+    handleEnabled(descendant)
+  elseif descendant:IsA("MeshPart") then
+    handleMeshPart(descendant)
+  elseif descendant:IsA("SpecialMesh") then
+    handleSpecialMesh(descendant)
+  elseif descendant:IsA("ShirtGraphic") then
+    handleShirtGraphic(descendant)
+  elseif descendant:IsA("Shirt") or descendant:IsA("Pants") then
+    handleClothing(descendant)
+  end
+end
+
+print("Anti-lag script loaded successfully!")
