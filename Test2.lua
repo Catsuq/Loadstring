9fdfab381a4af51dcb9fd755ea92e7d168bb724c
YellowGreg <101320329+YellowGreg@users.noreply.github.com>
2023-05-25 22:12:30 -0400

    Update Test2.lua

--- Test2.lua
+++ Test2.lua
@@ -1,57 +1,48 @@
 game.StarterGui:SetCore("ChatMakeSystemMessage",{
     Text = "NEW UPDATE!",
-    Color = Color3.fromRGB(255,165,0), 
-    Font = Enum.Font.Ubuntu
-})
-
+ Color = Color3.fromRGB(255,165,0), 
+ Font = Enum.Font.Ubuntu
+ })
 game.StarterGui:SetCore("ChatMakeSystemMessage",{
-    Text = "What's new for HarshTechV7.6",
-    Color = Color3.fromRGB(255,165,0), 
-    Font = Enum.Font.Ubuntu
-})
-
+    Text = " Whats new for HarshTechV7.5",
+ Color = Color3.fromRGB(255,165,0), 
+ Font = Enum.Font.Ubuntu
+ })
 game.StarterGui:SetCore("ChatMakeSystemMessage",{
     Text = "[+] New Scripts",
-    Color = Color3.fromRGB(0,255,0), 
-    Font = Enum.Font.Ubuntu
-})
-
+ Color = Color3.fromRGB(0,255,0), 
+ Font = Enum.Font.Ubuntu
+ })
 game.StarterGui:SetCore("ChatMakeSystemMessage",{
     Text = "[+] New Fe Scripts",
-    Color = Color3.fromRGB(0,255,0), 
-    Font = Enum.Font.Ubuntu
-})
-
+ Color = Color3.fromRGB(0,255,0), 
+ Font = Enum.Font.Ubuntu
+ })
 game.StarterGui:SetCore("ChatMakeSystemMessage",{
     Text = "[+] New Gui/Hub",
-    Color = Color3.fromRGB(0,255,0), 
-    Font = Enum.Font.Ubuntu
-})
-
+ Color = Color3.fromRGB(0,255,0), 
+ Font = Enum.Font.Ubuntu
+ })
 game.StarterGui:SetCore("ChatMakeSystemMessage",{
     Text = "[+] New Games",
-    Color = Color3.fromRGB(0,255,0), 
-    Font = Enum.Font.Ubuntu
-})
-
+ Color = Color3.fromRGB(0,255,0), 
+ Font = Enum.Font.Ubuntu
+ })
 game.StarterGui:SetCore("ChatMakeSystemMessage",{
     Text = "[+] Certain scripts can now be toggled, eliminating the need for you to reset yourself.",
     Color = Color3.fromRGB(0,255,0), 
     Font = Enum.Font.Ubuntu
 })
-
 game.StarterGui:SetCore("ChatMakeSystemMessage",{
     Text = "[+] Movable Toggle Button",
     Color = Color3.fromRGB(0,255,0), 
     Font = Enum.Font.Ubuntu
 })
-
 game.StarterGui:SetCore("ChatMakeSystemMessage",{
     Text = "[+] Added 'Our Script' Tab",
     Color = Color3.fromRGB(255,0,10), 
     Font = Enum.Font.Ubuntu
 })
-
 game.StarterGui:SetCore("ChatMakeSystemMessage",{
     Text = "[-] Removed some non-functional Fe scripts",
     Color = Color3.fromRGB(255,0,0), 
